e47ed0f0b6c5f1ad64d653bbd78cd254d15a9837
Denise Carpenter <dcarpenter88@host-60-156-73-167.ussignalcom.net>
2012-11-17 14:09:53 -0500

    Finished first cut of the User model

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
-source 'https://rubygems.org'
+source 'http://rubygems.org'
 
 gem "rails"
 
@@ -26,6 +26,7 @@ group :assets do
 end
 
 gem 'jquery-rails'
+
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,13 +4,16 @@ describe UsersController do
   render_views
 
   describe "GET 'new'" do
+
     it "should be successful" do
       get 'new'
       response.should be_success
     end
-  it "should have the right title" do
+
+    it "should have the right title" do
       get 'new'
-      response.should have_selector ("title", :content => "sign up")
+      response.should have_selector("title", :content => "sign up")
     end
+
   end
 end
