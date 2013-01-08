944113cb780d14b45618f78fcd6c770a5a5cb41f
Denise Carpenter <dcarpenter88@Denises-MacBook-Pro.local>
2013-01-07 20:54:02 -0500

    Done with sign in

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "LayoutLinks" do
+describe "Layout links" do
 
   it "should have a Home page at'/'" do
     get '/'
@@ -22,4 +22,28 @@ describe "LayoutLinks" do
       response.should have_selector('title', :content => "Help")
   end
 
+  describe "when not signed in" do
+    it "should have a Sign in link"do
+      visit root_path
+      response.should have_selector("a", :href => signin_path,
+                                         :content => "Sign in")
+    end
+  end
+
+  describe "when signed in" do
+
+    before(:each)do
+      @user + Factory(:user)
+      visit signin_path
+      fill_i n :email,    :with => @user.email
+      fill_in :password,  :with => @user.password
+      click_button
+    end
+
+    it "should have a profile link"do
+      visit root_path
+      response.should have_selector("a", :href => user_path(@user),
+                                         :content => "profile")
+    end
+  end
 end
